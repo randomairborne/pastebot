658982693c06ddda6d10bc2df37ad61412d8d118
valkyrie_pilot <valk@randomairborne.dev>
2022-05-16 21:22:21 +0000

    updoots

--- site/src/main.rs
+++ site/src/main.rs
@@ -13,52 +13,38 @@ use types::{Css, JavaScript, Png};
 
 const BOT_INVITE: &str = "https://discord.com/api/oauth2/authorize?client_id=975460814007963738&permissions=0&scope=applications.commands%20bot";
 
-struct Files {
-    paste: String,
-    index: String,
-    css: String,
-    highlight_js: String,
-    highlight_css: String,
-    logo: Vec<u8>,
-}
-
 #[tokio::main]
 async fn main() {
-    let files = Files {
-        paste: std::fs::read_to_string("resources/paste.html").unwrap(),
-        index: std::fs::read_to_string("resources/index.html").unwrap(),
-        css: std::fs::read_to_string("resources/main.css").unwrap(),
-        highlight_js: std::fs::read_to_string("resources/highlight.js").unwrap(),
-        highlight_css: std::fs::read_to_string("resources/highlight.css").unwrap(),
-        logo: std::fs::read("resources/logo.png").unwrap(),
-    };
-
+    let paste = std::fs::read("resources/paste.html").unwrap();
+    let index = std::fs::read("resources/index.html").unwrap();
+    let about = std::fs::read("resources/about.html").unwrap();
+    let privacy = std::fs::read("resources/privacy.html").unwrap();
+    let terms = std::fs::read("resources/terms.html").unwrap();
+    let css = std::fs::read("resources/main.css").unwrap();
+    let highlight_js = std::fs::read("resources/highlight.js").unwrap();
+    let highlight_css = std::fs::read("resources/highlight.css").unwrap();
+    let logo = std::fs::read("resources/logo.png").unwrap();
     let app = axum::Router::new()
-        .route("/", get(move || async { Html(files.index) }))
+        .route("/", get(move || async { Html(index) }))
         .route(
             "/invite",
             get(move || async { Redirect::permanent(BOT_INVITE) }),
         )
+        .route("/about", get(move || async { Html(about) }))
         .route(
             "/:channelid/:messageid/:filename",
-            get(move || async { Html(files.paste) }),
+            get(move || async { Html(paste) }),
         )
-        .route(
-            "/css/main.css",
-            get(move || async { Css(files.css) } )
-            )
+        .route("/css/main.css", get(move || async { Css(css) }))
         .route(
             "/js/highlight.js",
-            get(move || async { JavaScript(files.highlight_js) }),
+            get(move || async { JavaScript(highlight_js) }),
         )
         .route(
             "/css/highlight.css",
-            get(move || async { Css(files.highlight_css) }),
+            get(move || async { Css(highlight_css) }),
         )
-        .route(
-            "/logo.png", 
-            get(move || async { Png(files.logo) })
-            );
+        .route("/logo.png", get(move || async { Png(logo) }));
 
     let listen = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("[INFO] Listening on http://{}", &listen);
