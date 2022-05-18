ade8a3a5e39c090e8600ffdca50a3a79ff4774a0
valkyrie_pilot <valk@randomairborne.dev>
2022-05-17 20:49:39 -0600

    remove asref call

--- bot/src/event.rs
+++ bot/src/event.rs
@@ -24,7 +24,7 @@ pub async fn message(ctx: Context, msg: Message) -> Result<(), serenity::Error> 
     for attachment in &msg.attachments { 
         if let Some(ctype) = attachment.content_type {
             if let Some(charset) = ctype.split("; ").collect::<Vec<&str>>().get(1) {
-            if charset != "charset=utf-8".as_ref() {
+            if charset != &"charset=utf-8" {
                     continue;
                 }
             }
