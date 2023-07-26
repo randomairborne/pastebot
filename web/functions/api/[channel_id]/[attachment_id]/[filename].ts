bbb66385a8c9f52e61b0702c166eb966cf6eccdb
valkyrie_pilot <valk@randomairborne.dev>
2023-07-26 16:05:13 -0600

    add user agent

--- web/functions/api/[channel_id]/[attachment_id]/[filename].ts
+++ web/functions/api/[channel_id]/[attachment_id]/[filename].ts
@@ -4,7 +4,12 @@ export const onRequest: PagesFunction = async ({ params }) => {
   let url = `https://cdn.discordapp.com/attachments/${params.channel_id}/${params.attachment_id}/${params.filename}`;
   console.log(url);
   let response = await fetch(
-    url
+    url,
+    {
+        headers: {
+            "User-Agent": "pastebot/0.1 (+https://paste.valk.sh)"
+        }
+    }
   );
   let body = await response.arrayBuffer();
   const dec = new TextDecoder("utf-8");
